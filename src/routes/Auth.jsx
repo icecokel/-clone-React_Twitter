2c716807161283bbdaf846657918d5593f8372b4
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-02-12 13:33:47 +0900

    로그인 구현중

--- src/routes/Auth.jsx
+++ src/routes/Auth.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => (
+  <div>
+    <form action="">
+      <input type="text" placeholder="Email" />
+      <input type="password" name="" id="" placeholder="Password" />
+      <input type="submit" value="Log In" />
+    </form>
+
+    <div>
+      <button> Coutinue With Google</button>
+    </div>
+  </div>
+);
 
 export default Auth;
