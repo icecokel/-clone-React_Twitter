7f0f7ee901fa35c6ef75ff27b0a1b9e087eb2b36
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-02-13 15:13:02 +0900

    로그인 구현중

--- src/routes/Auth.jsx
+++ src/routes/Auth.jsx
@@ -1,17 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import { authService } from "../fbUtil";
 
-const Auth = () => (
-  <div>
-    <form action="">
-      <input type="text" placeholder="Email" />
-      <input type="password" name="" id="" placeholder="Password" />
-      <input type="submit" value="Log In" />
-    </form>
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
 
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+
+    if (name === "email") {
+      setEmail(value);
+    }
+    if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      // 게정 파라미터 형태 { email : string , password : string }
+      let data = "";
+      if (newAccount) {
+        // Create Account
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+      } else {
+        // Log in
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
     <div>
-      <button> Coutinue With Google</button>
+      <form onSubmit={onSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+      </form>
+
+      <div>
+        <button> Coutinue With Google</button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Auth;
