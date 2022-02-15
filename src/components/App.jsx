0d695f52fd2f277b86a35498294baefdc6bcfd73
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-02-15 23:04:07 +0900

    로그인 처리

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AppRouter from "./Router";
 import { authService } from "../fbUtil";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function App() {
+  const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
 
+  useEffect(() => {
+    const auth = getAuth();
+    !init &&
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setIsLoggedIn(true);
+        } else {
+          setIsLoggedIn(false);
+        }
+        setInit(true);
+      });
+  }, []);
   return (
     <>
       <AppRouter isLoggedIn={isLoggedIn} />
